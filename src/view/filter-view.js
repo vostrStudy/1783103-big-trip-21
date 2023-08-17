af4777f4d9eb0b2d705656796055a5df03fac47d
id_studyaccount <vostr.study@gmail.com>
2023-08-17 22:25:26 +0800

    Время разбивать камни

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -5,14 +5,14 @@ export default class FilterView {
   getTemplate() {
     return createFilterTemplate();
   }
- 
+
   getElement() {
     if (!this.element) {
       this.element = createElement(this.getTemplate());
     }
     return this.element;
   }
-  
+
   removeElement() {
     this.element = null;
   }
