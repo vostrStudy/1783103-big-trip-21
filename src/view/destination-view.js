dd61fbb10f5ec6863cc2c9a943b58e3164842c0b
id_studyaccount <vostr.study@gmail.com>
2023-09-11 11:09:29 +0800

    Шаблонизируй это

--- src/view/destination-view.js
+++ src/view/destination-view.js
@@ -2,25 +2,23 @@ import {createElement} from '../render.js';
 import {createDestinationTemplate} from '../template/destiation-template.js';
 
 export default class DestinationView {
-  
+
   constructor({pointDestination}){
     this.pointDestination = pointDestination;
   }
-    getTemplate() {
-        return createDestinationTemplate(this.pointDestination);
-      }
-    
-      getElement(){
-        if(!this.element){
-          this.element = createElement(this.getTemplate());
-        }
-        return this.element;
-      }
-    
-      removeElement(){
-        this.element = null;
-      }
 
+  getTemplate(){
+    return createDestinationTemplate(this.pointDestination);
+  }
 
+  getElement(){
+    if(!this.element){
+      this.element = createElement(this.getTemplate());
     }
-  
+    return this.element;
+  }
+
+  removeElement(){
+    this.element = null;
+  }
+}
